aa2a8acb3a7014bd87d67f81c3539d6a4700d5e2
Jekotia <contact@jekotia.net>
2019-06-19 13:55:35 -0400

    Change init to need to be called. Exit if a file source operation fails. Basic example.

---
+++ examples/bootstrap.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# Source /functions/init ; modify the path to libbash to suit your environment.
+source "${HOME}/github/libbash/functions/init"
+
+# Invoke the init. JLB_ROOT will have been set when the init was sourced.
+jlb::init "${JLB_ROOT}"
